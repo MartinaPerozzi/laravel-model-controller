1a63cb7aa9eaedee9a6e4623293ef5cd8b61721e
MartinaPerozzi <perozzimartina@gmail.com>
2023-04-06 14:18:21 +0200

    + correction comment

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function index()
     {
-        // Metto in un array di oggetti tutte le case della tabelline
+        // Metto in un array di oggetti tutte i movie della tabella
         $movies = Movie::all();
 
         return view('movies.list', compact('movies'));
